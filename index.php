6132ab4dfc16f1dbe20aa784036c16124900f447
jeanncarillo <36177061+jeanncarillo@users.noreply.github.com>
2018-02-06 10:21:18 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+<title>SELF INFORMATION</title>
+</head>
+<body bgcolor="#fffacc">
+<center><h1>MY FIRST PROJECT</h1></center>
+<h2>PERSONAL DATA</h2>
+<h4>Name:<u> Jean M. Carillo</u></h4></p>
+<p><h4>Address:<u> Tunghaan, Minglanilla, Cebu</u></h4></p>
+<p><h4>Contact:<u> 0942089938</u></h4></p>
+<p><h4>Email:<u> jeanzcarillo@yahoo.com</u></h4></p>
+<p><h4>Age:<u> 22</u></h4></p>
+<p><h4>Gender:<u> Female</u></h4></p>
+<p><h4>D.O.B.:<U> October 03 1995</u></h4></p>
+<p><h4>Father's Name:<u> Nelson Carillo</u></h4></p>
+<p><h4>Mother's Name:<u> Jeanny Vi Carillo</u></h4></p>
+<p><h4>Siblings:<u> 3 Sisters </u></h4></p>
+<h2>EDUCATIONAL BACKGROUND</h2>
+<p><h4>Elementary:<u> Tagbay Elementary School</u></h4></p>
+<p><h4>High School:<u> Del Monte National High School</u></h4></p>
+<p><h4>College:<u> CTU-Naga Extension Campus</u></h4></p>
+<center><h2>BIOGRAPHY</h2></center>
+ 
+<p><h4><br>When I was 13 years old,we do not have gadget to use for.Our entertainment was only radio because my parents
+cannot afford to buy gadget,at that time gadget is expensive. There was a time when I was in our classroom,some of 
+my classmates were playing their expensive phone,they were very happy. I feel jelous at that time but I did not express 
+my feelings toward them.<br>
+ 
+<br>Since I like to have such thing,I said to my father that;father I want to have touchscreen phone,please brought phone 
+for me. But my father answered;"Jean, please be patient as of now I don't have enough money to brought that phone,understand 
+me ok".So I shared to my friend Jessa that I want to learn some basic function of touchscreen phone. At that time I really
+happy because she teached me some basic funtion. And I'm thankfull to her that she help me.Seven years later,I realized 
+that I need more learning regarding technology.I tried my best that someday I will become good technologian.<br>
+<br>Presently,I now studying in CTU-Naga Extension Campus with the course of Batchelor of Science Information Communication 
+Technology(BSICT). 
+<br</p><h4>
+</body>
+</html>
